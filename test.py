ed27f10c33f9945c8189deb62b9a0641b96964d9
Diri <diri@naver.com>
2018-02-28 15:41:44 +0900

    name change

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-def fibo(n):
-    return tail_fibo(n, 0, 1)
-    
-def tail_fibo(n, current, next):
-    if n == 0:
-        return current
-    else:
-        return tail_fibo(n-1, next, current + next)
+def spiral(a, b):
+    Matrix = [[0]*b for i in range(a)]
+    index = 0
+
+    for i in range(a):
+        for j in range(b):
+            Matrix[i][j] = index
+            index += 1
+
+    for i in range(a):
+        for j in range(b):
+            if a == 0 and b == 0:
+                pass
+
+    for i in range(a):
+        print(Matrix[i])
+
 
 if __name__ == '__main__':
-    n = int(input())
-    print(fibo(n))
+    spiral(4,5)
+
+#Retry
