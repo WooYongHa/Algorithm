166ec3bf12c5e47c8906c1b5f5de228d99d63b20
Diri <diri@naver.com>
2018-03-03 16:45:31 +0900

    Algo12

---
+++ Algo12.py
@@ -0,0 +1,21 @@
+# 모든 짝수번째 숫자를 * 로 치환하시오.(홀수번째 숫자,또는 짝수번째 문자를 치환하면 안됩니다.)
+# lv 2
+s = "a1b2cde3~g45hi6"
+a = ['a',1,'b',2,'c','d','e',3,'~','g',4,5,'h','i',6]
+
+def changeNum():
+    for i in range(len(a)):
+        if i%2 !=0  and type(a[i]) is int:
+            a[i] = '*'
+    print(a)
+
+def cnvt(s):
+    l = list(s)
+    for k in range(1, len(l), 2):
+        if l[k].isdigit():
+            l[k] = '*'
+    return ''.join(l)
+
+if __name__ == "__main__":
+    # changeNum()
+    print(cnvt(s))
