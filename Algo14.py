bdbcf17d579b04ef09e67d9a49f2a25941caa24b
WooYongHa <wyh951753@gmail.com>
2018-03-05 10:46:06 +0900

    Algo14

---
+++ Algo14.py
@@ -0,0 +1,17 @@
+'''
+Insertion sort
+'''
+# lv2
+
+a = [5, 2, 4, 6, 1, 3]
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i):
+            while arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+if __name__ == '__main__':
+    print(insertionSort(a))
+
