bccc2f97d01aa46257fcf44a8a3668bba67b0070
WooYongHa <wyh951753@gmail.com>
2018-03-07 17:42:05 +0900

    Algo16(Sorting)

---
+++ Algo16.py
@@ -0,0 +1,13 @@
+'''
+Sorting
+            Best    Worst
+버블 정렬       n       n^2
+선택 정렬       n^2     n^2
+삽입 정렬       n       n^2
+퀵   정렬      nlogn   nlogn~n^2
+
+'''
+
+# Code
+# http://ejklike.github.io/2017/03/04/sorting-algorithms-with-python.html
+
